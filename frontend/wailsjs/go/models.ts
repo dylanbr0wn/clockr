7ff2bb837cc5395e0f0e6082457ed40e781eb6f2
Dylan Brown <dylan.bro97@gmail.com>
2026-07-09 14:50:54 -0700

    chore(wails): regenerate ExcludeEvent bindings

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -391,6 +391,34 @@ export namespace service {
 	        this.Providers = source["Providers"];
 	    }
 	}
+	export class ExcludeEventInput {
+	    eventId: number;
+	    periodId: number;
+	
+	    static createFrom(source: any = {}) {
+	        return new ExcludeEventInput(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.eventId = source["eventId"];
+	        this.periodId = source["periodId"];
+	    }
+	}
+	export class ExcludeEventResult {
+	    periodId: number;
+	    eventId: number;
+	
+	    static createFrom(source: any = {}) {
+	        return new ExcludeEventResult(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.periodId = source["periodId"];
+	        this.eventId = source["eventId"];
+	    }
+	}
 	export class GapFill {
 	    id: number;
 	    periodId: number;
@@ -500,34 +528,6 @@ export namespace service {
 		}
 	}
 	
-	export class ExcludeEventInput {
-	    eventId: number;
-	    periodId: number;
-	
-	    static createFrom(source: any = {}) {
-	        return new ExcludeEventInput(source);
-	    }
-	
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.eventId = source["eventId"];
-	        this.periodId = source["periodId"];
-	    }
-	}
-	export class ExcludeEventResult {
-	    periodId: number;
-	    eventId: number;
-	
-	    static createFrom(source: any = {}) {
-	        return new ExcludeEventResult(source);
-	    }
-	
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.periodId = source["periodId"];
-	        this.eventId = source["eventId"];
-	    }
-	}
 	export class ManualEventDeleteInput {
 	    id: number;
 	    periodId: number;
